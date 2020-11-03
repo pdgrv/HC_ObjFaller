9ce9938c307140701f710e199abd3972a6404ecf
Maxim Podgorov <mapodgorov@gmail.com>
2020-11-03 17:45:40 +0700

    gui fixes anchors

--- Assets/Scripts/UI/StartingMenu.cs
+++ Assets/Scripts/UI/StartingMenu.cs
@@ -7,8 +7,8 @@ public class StartingMenu : MonoBehaviour
 {
     private void Update()
     {
-        //if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) -- для билда
-        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) //-- для билда
+        //if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
             gameObject.SetActive(false);
     }
 }
