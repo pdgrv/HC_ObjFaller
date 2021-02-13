5a9e3d0b40caa0b34303529479e13f3ef96aedd3
Maxim Podgorov <mapodgorov@gmail.com>
2021-02-13 16:39:19 +0700

    sss

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -46,11 +46,6 @@ public class GameManager : MonoBehaviour
         _levelGenerator.PlatformCountChanged -= OnPlatformCountChanged;
     }
 
-    public void RestartLevel()
-    {
-        LoadScene();
-    }
-
     public void StartLevel()
     {
         LoadScene();
